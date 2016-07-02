568695995a6e1185fecd7f23eba1bd04895c5cbf
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-07-02 10:05:48 -0500

    authorization fix

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,8 +1,15 @@
 const authorize = (req, res, next) => {
   const user = req.user
+  const app = req.headers['x-access-app']
 
-  if (user.apps.indexOf(req.body.app) === -1) {
-    res.status(401).json({ message: `${user.name} has no access to this web app.` })
+  if (app) {
+    if (user.apps.indexOf(app) === -1) {
+      res.status(401).json({ message: `${user.name} has no access to this web app.` })
+    }
+  } else {
+    res.status(400).json({
+      message: 'No target app provided'
+    })
   }
 }
 
