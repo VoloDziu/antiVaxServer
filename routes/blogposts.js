32d0765ba35cd2bc6cc61f34d82a85f6ab6e1f0c
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-07-14 09:13:51 -0500

    added blogpost comments

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -2,6 +2,7 @@ var express = require('express')
 var ObjectId = require('mongoose').Types.ObjectId
 
 var Blogpost = require('../models/blogpost')
+var Comment = require('../models/comment')
 var isRegistered = require('../middleware/authorization').isRegistered
 var isAdmin = require('../middleware/authorization').isAdmin
 
@@ -103,8 +104,94 @@ blogpostRoutes.post('/', isRegistered, isAdmin, (req, res) => {
   })
 })
 
-// TODO: blogpost comments routes:
-// POST /comments/
-// PUT /comments/:id
+// Delete
+blogpostRoutes.delete('/:blogpostId', isRegistered, isAdmin, (req, res) => {
+  Blogpost.findOne({_id: ObjectId(req.params.blogpostId)})
+    .then(blogpost => {
+      if (blogpost) {
+        blogpost.remove()
+
+        res.json({
+          success: true,
+          data: {}
+        })
+      } else {
+        res.status(404).json({
+          success: false,
+          data: {}
+        })
+      }
+    })
+})
+
+// Create comment
+blogpostRoutes.post('/:blogpostId/comments/', isRegistered, (req, res) => {
+  Blogpost.findOne({_id: ObjectId(req.params.blogpostId)})
+    .then(blogpost => {
+      if (blogpost) {
+        var comment = new Comment(Object.assign({}, req.body.comment, {
+          lastModifiedBy: req.user.name,
+          lastModifiedAt: Date.now(),
+          createdAt: Date.now()
+        }))
+
+        var nComments = blogpost.comments.length
+
+        blogpost.comments.push(comment)
+
+        blogpost.save((err, blogpost) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              data: err
+            })
+          } else {
+            res.json({
+              success: true,
+              data: {
+                comment: blogpost.comments[nComments]
+              }
+            })
+          }
+        })
+      } else {
+        res.status(404).json({
+          success: false,
+          data: {}
+        })
+      }
+    })
+})
+
+// Delete comment
+blogpostRoutes.post('/:blogpostId/comments/:commentId', isRegistered, (req, res) => {
+  Blogpost.findOne({_id: ObjectId(req.params.blogpostId)})
+    .then(blogpost => {
+      if (blogpost) {
+        blogpost = Object.assign({}, blogpost, {
+          comments: blogpost.comments.filter(c => c._id !== ObjectId(req.params.commentId))
+        })
+
+        blogpost.save((err, blogpost) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              data: err
+            })
+          } else {
+            res.json({
+              success: true,
+              data: {}
+            })
+          }
+        })
+      } else {
+        res.status(404).json({
+          success: false,
+          data: {}
+        })
+      }
+    })
+})
 
 module.exports = blogpostRoutes
