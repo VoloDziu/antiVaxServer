5ba916c847467a0ca8b0ce2658969f7ecd263ede
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2019-08-27 10:11:28 -0500

    fixed mongoose issues

--- antiVaxServer.js
+++ antiVaxServer.js
@@ -1,24 +1,26 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var morgan = require('morgan')
-var mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-var authRoutes = require('./routes/auth')
-var usersRoutes = require('./routes/users')
-var questionsRoutes = require('./routes/questions')
-var articleRoutes = require('./routes/articles')
-var scheduleRoutes = require('./routes/schedule')
-var searchIndexRoutes = require('./routes/searchIndex')
+const authRoutes = require('./routes/auth');
+const usersRoutes = require('./routes/users');
+const questionsRoutes = require('./routes/questions');
+const articleRoutes = require('./routes/articles');
+const scheduleRoutes = require('./routes/schedule');
+const searchIndexRoutes = require('./routes/searchIndex');
 
-var port = process.env.PORT || 3000
-mongoose.connect(`mongodb://${process.env.ANTIVAX_SERVER_DB_USER}:${process.env.ANTIVAX_SERVER_DB_PASS}@${process.env.ANTIVAX_SERVER_DB_HOST}/${process.env.ANTIVAX_SERVER_DB_NAME}`)
+const port = process.env.PORT || 3000;
+mongoose.connect(`mongodb://${process.env.ANTIVAX_SERVER_DB_USER}:${process.env.ANTIVAX_SERVER_DB_PASS}@${process.env.ANTIVAX_SERVER_DB_HOST}/${process.env.ANTIVAX_SERVER_DB_NAME}`, {
+  useMongoClient: true
+});
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // use morgan to log requests to the console
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -26,7 +28,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, OPTIONS')
   res.header('Allow', 'GET, HEAD, POST, PUT, DELETE, OPTIONS')
   next()
-})
+});
 
 app.get(`${process.env.ANTIVAX_SERVER_API_PREFIX}/`, (req, res) => {
   res.json({
@@ -34,13 +36,14 @@ app.get(`${process.env.ANTIVAX_SERVER_API_PREFIX}/`, (req, res) => {
     data: {
       status: 'working'
     }
-  })
-})
+  });
+});
 
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/auth`, authRoutes)
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/users`, usersRoutes)
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/questions`, questionsRoutes)
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/articles`, articleRoutes)
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/schedule`, scheduleRoutes)
-app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/searchIndex`, searchIndexRoutes)
-app.listen(port)
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/auth`, authRoutes);
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/users`, usersRoutes);
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/questions`, questionsRoutes);
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/articles`, articleRoutes);
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/schedule`, scheduleRoutes);
+app.use(`${process.env.ANTIVAX_SERVER_API_PREFIX}/searchIndex`, searchIndexRoutes);
+
+app.listen(port, () => console.log(`Vaccine Answers Server is now active on port ${port}`));
